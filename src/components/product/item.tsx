cef759a8c69d410133eb3730bfd723955d16c999
sangmi <jude4264@gmail.com>
2022-12-16 17:58:02 +0900

    [UPDATE] Setting

---
+++ src/components/product/item.tsx
@@ -0,0 +1,13 @@
+import { Product } from "../../types";
+
+const ProductItem = ({
+  category,
+  description,
+  id,
+  image,
+  price,
+  rating,
+  title,
+}: Product) => <div>상세페이지</div>;
+
+export default ProductItem;
